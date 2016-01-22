b3b406b6db2f93b1bb6d119aa6d794bfadfa7e32
iceStone <ice@micua.com>
2016-01-22 17:08:34 +0800

    step-04 调用豆瓣API实现动态数据获取

--- app.js
+++ app.js
@@ -2,196 +2,10 @@
  * @Author: iceStone
  * @Date:   2016-01-22 15:57:55
  * @Last Modified by:   iceStone
- * @Last Modified time: 2016-01-22 16:25:08
+ * @Last Modified time: 2016-01-22 17:07:08
  */
 
 'use strict';
-var data = [{
-  "rating": {
-    "max": 10,
-    "average": 7.3,
-    "stars": "40",
-    "min": 0
-  },
-  "genres": [
-    "动作",
-    "科幻",
-    "奇幻"
-  ],
-  "collect_count": 83865,
-  "casts": [{
-    "avatars": {
-      "small": "https://img1.doubanio.com/img/celebrity/small/1398825610.2.jpg",
-      "large": "https://img1.doubanio.com/img/celebrity/large/1398825610.2.jpg",
-      "medium": "https://img1.doubanio.com/img/celebrity/medium/1398825610.2.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1339916/",
-    "id": "1339916",
-    "name": "黛西·雷德利"
-  }, {
-    "avatars": {
-      "small": "https://img3.doubanio.com/img/celebrity/small/53065.jpg",
-      "large": "https://img3.doubanio.com/img/celebrity/large/53065.jpg",
-      "medium": "https://img3.doubanio.com/img/celebrity/medium/53065.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1322327/",
-    "id": "1322327",
-    "name": "亚当·德赖弗"
-  }, {
-    "avatars": {
-      "small": "https://img3.doubanio.com/img/celebrity/small/23541.jpg",
-      "large": "https://img3.doubanio.com/img/celebrity/large/23541.jpg",
-      "medium": "https://img3.doubanio.com/img/celebrity/medium/23541.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1009238/",
-    "id": "1009238",
-    "name": "哈里森·福特"
-  }],
-  "title": "星球大战：原力觉醒",
-  "original_title": "Star Wars: The Force Awakens",
-  "subtype": "movie",
-  "directors": [{
-    "avatars": {
-      "small": "https://img1.doubanio.com/img/celebrity/small/1779.jpg",
-      "large": "https://img1.doubanio.com/img/celebrity/large/1779.jpg",
-      "medium": "https://img1.doubanio.com/img/celebrity/medium/1779.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1010542/",
-    "id": "1010542",
-    "name": "J·J·艾布拉姆斯"
-  }],
-  "year": "2015",
-  "images": {
-    "small": "https://img1.doubanio.com/view/movie_poster_cover/ipst/public/p2293954118.jpg",
-    "large": "https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2293954118.jpg",
-    "medium": "https://img1.doubanio.com/view/movie_poster_cover/spst/public/p2293954118.jpg"
-  },
-  "alt": "http://movie.douban.com/subject/20326665/",
-  "id": "20326665"
-}, {
-  "rating": {
-    "max": 10,
-    "average": 7.6,
-    "stars": "40",
-    "min": 0
-  },
-  "genres": [
-    "剧情",
-    "传记",
-    "冒险"
-  ],
-  "collect_count": 13918,
-  "casts": [{
-    "avatars": {
-      "small": "https://img1.doubanio.com/img/celebrity/small/3517.jpg",
-      "large": "https://img1.doubanio.com/img/celebrity/large/3517.jpg",
-      "medium": "https://img1.doubanio.com/img/celebrity/medium/3517.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1101703/",
-    "id": "1101703",
-    "name": "约瑟夫·高登-莱维特"
-  }, {
-    "avatars": {
-      "small": "https://img1.doubanio.com/img/celebrity/small/1374649659.58.jpg",
-      "large": "https://img1.doubanio.com/img/celebrity/large/1374649659.58.jpg",
-      "medium": "https://img1.doubanio.com/img/celebrity/medium/1374649659.58.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1054393/",
-    "id": "1054393",
-    "name": "本·金斯利"
-  }, {
-    "avatars": {
-      "small": "https://img3.doubanio.com/img/celebrity/small/1389083702.11.jpg",
-      "large": "https://img3.doubanio.com/img/celebrity/large/1389083702.11.jpg",
-      "medium": "https://img3.doubanio.com/img/celebrity/medium/1389083702.11.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1330092/",
-    "id": "1330092",
-    "name": "夏洛特·勒·邦"
-  }],
-  "title": "云中行走",
-  "original_title": "The Walk",
-  "subtype": "movie",
-  "directors": [{
-    "avatars": {
-      "small": "https://img3.doubanio.com/img/celebrity/small/505.jpg",
-      "large": "https://img3.doubanio.com/img/celebrity/large/505.jpg",
-      "medium": "https://img3.doubanio.com/img/celebrity/medium/505.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1053564/",
-    "id": "1053564",
-    "name": "罗伯特·泽米吉斯"
-  }],
-  "year": "2015",
-  "images": {
-    "small": "https://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2299209970.jpg",
-    "large": "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2299209970.jpg",
-    "medium": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2299209970.jpg"
-  },
-  "alt": "http://movie.douban.com/subject/25819229/",
-  "id": "25819229"
-}, {
-  "rating": {
-    "max": 10,
-    "average": 6.3,
-    "stars": "35",
-    "min": 0
-  },
-  "genres": [
-    "犯罪",
-    "悬疑"
-  ],
-  "collect_count": 899,
-  "casts": [{
-    "avatars": {
-      "small": "https://img3.doubanio.com/img/celebrity/small/32971.jpg",
-      "large": "https://img3.doubanio.com/img/celebrity/large/32971.jpg",
-      "medium": "https://img3.doubanio.com/img/celebrity/medium/32971.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1031194/",
-    "id": "1031194",
-    "name": "任达华"
-  }, {
-    "avatars": {
-      "small": "https://img3.doubanio.com/img/celebrity/small/1414655610.66.jpg",
-      "large": "https://img3.doubanio.com/img/celebrity/large/1414655610.66.jpg",
-      "medium": "https://img3.doubanio.com/img/celebrity/medium/1414655610.66.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1015713/",
-    "id": "1015713",
-    "name": "霍建华"
-  }, {
-    "avatars": {
-      "small": "https://img3.doubanio.com/img/celebrity/small/25841.jpg",
-      "large": "https://img3.doubanio.com/img/celebrity/large/25841.jpg",
-      "medium": "https://img3.doubanio.com/img/celebrity/medium/25841.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1037420/",
-    "id": "1037420",
-    "name": "张赫"
-  }],
-  "title": "真相禁区",
-  "original_title": "真相禁区",
-  "subtype": "movie",
-  "directors": [{
-    "avatars": {
-      "small": "https://img1.doubanio.com/img/celebrity/small/1380274419.77.jpg",
-      "large": "https://img1.doubanio.com/img/celebrity/large/1380274419.77.jpg",
-      "medium": "https://img1.doubanio.com/img/celebrity/medium/1380274419.77.jpg"
-    },
-    "alt": "http://movie.douban.com/celebrity/1320018/",
-    "id": "1320018",
-    "name": "麦咏麟"
-  }],
-  "year": "2016",
-  "images": {
-    "small": "https://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2307814476.jpg",
-    "large": "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2307814476.jpg",
-    "medium": "https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2307814476.jpg"
-  },
-  "alt": "http://movie.douban.com/subject/26348798/",
-  "id": "26348798"
-}];
 
 
 /**
@@ -201,6 +15,26 @@ var data = [{
  */
 var moviecatApp = angular.module('MoviecatApp', []);
 
-moviecatApp.controller('MainController', ['$scope', function($scope) {
-  $scope.hotMovies = data;
+/*服务的URL配置*/
+moviecatApp.constant('ApiAddress', {
+  hot_movies: 'https://api.douban.com/v2/movie/in_theaters',
+});
+
+moviecatApp.controller('MainController', ['$scope', '$http', 'ApiAddress', function($scope, $http, ApiAddress) {
+  // $scope.hotMovies = [];
+
+  // 由于 douban API 不支持 angular.callbacks._0 这种类型的 callback 名称，所以不能直接使用 JSONP
+  // $http({
+  //   method: 'JSONP',
+  //   url: APIURL
+  // }).success(function(data) {
+  //   $scope.hotMovies = data.subjects;
+  // }).error(function(error) {
+  // });
+
+  // 将回到函数挂到window对象下
+  window.hotMoviesCallback = function(data) {
+    $scope.hotMovies = data.subjects;
+  };
+  $http.jsonp(ApiAddress.hot_movies + '?callback=hotMoviesCallback');
 }]);
