93262226c3707e31a7095ad09c179e9b8f20ab39
iceStone <ice@micua.com>
2016-01-22 15:58:52 +0800

    step-01 构建静态结构

---
+++ README.md
@@ -0,0 +1,5 @@
+# Learning Angular
+
+## 构建项目结构
+
+http://developers.douban.com/wiki/?title=api_v2
---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+* @Author: iceStone
+* @Date:   2016-01-22 15:57:55
+* @Last Modified by:   iceStone
+* @Last Modified time: 2016-01-22 15:58:14
+*/
+
+'use strict';
